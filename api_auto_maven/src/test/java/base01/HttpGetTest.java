4d525ea376733fa1eb13a1a583c3aeb95cd65969
caicai <915758016@qq.com>
2019-05-14 17:27:21 +0800

    321

---
+++ api_auto_maven/src/test/java/base01/HttpGetTest.java
@@ -0,0 +1,49 @@
+package base01;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/**
+ * @Desc:基本get请求 
+ * @author:zpp 
+ * @time:2019年5月14日 下午5:02:12
+ */
+public class HttpGetTest {
+	public static void main(String[] args){
+		try {
+			//接口地址
+			String url = "http://120.78.128.25:8080/futureloan/mvc/api/member/register?mobilephone=&pwd=";
+			//get请求
+			HttpGet get = new HttpGet(url);
+			//客户端
+			CloseableHttpClient client = HttpClients.createDefault();
+			//执行get请求，得到响应信息
+			CloseableHttpResponse response = client.execute(get);
+			//状态行信息
+			StatusLine statusLine = response.getStatusLine();
+			System.out.print(statusLine.getProtocolVersion() + " ");
+			System.out.print(statusLine.getStatusCode() + " ");
+			System.out.println(statusLine.getReasonPhrase());
+			//响应头信息
+			Header[] allHeaders = response.getAllHeaders();
+			for (Header header : allHeaders) {
+				System.out.println(header.getName() + ":" + header.getValue());
+			}
+			//空行
+			System.out.println();
+			//响应体信息
+			HttpEntity entity = response.getEntity();
+			String entityStr = EntityUtils.toString(entity);
+			System.out.println(entityStr);
+	
+		} catch (Exception e) {			
+			e.printStackTrace();
+		}
+	}
+}
--- web_auto_maven/src/main/java/web/base02/New_Test_Case03.java
+++ web_auto_maven/src/main/java/web/base02/New_Test_Case03.java
@@ -1,11 +1,15 @@
 package web.base02;
 
+import java.sql.Time;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.log4j.Logger;
+import org.eclipse.jetty.util.statistic.SampleStatistic;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -16,6 +20,8 @@ import org.testng.annotations.Test;
  * @time:2019年5月13日 下午5:19:02
  */
 public class New_Test_Case03 extends BaseBrowserAttrs{
+	private static Logger logger = Logger.getLogger(New_Test_Case03.class);
+	
 	//登陆成功，显示等待
 	//@Test
 	public void test_case_01(){
@@ -119,38 +125,60 @@ public class New_Test_Case03 extends BaseBrowserAttrs{
 	@Test
 	public void test_case_06(){
 		toPage("http://test.lemonban.com/lmcanon_web_auto/mng/login.html");
+		
 		WebDriver driver = getWebDriver();
+		
+		logger.info("输入账号");
 		driver.findElement(By.id("mobilephone")).sendKeys("13825161923");;
+		
+		logger.info("输入密码");
 		driver.findElement(By.id("password")).sendKeys("123456");
+		
+		logger.info("点击登录按钮");
 		driver.findElement(By.className("btn-success")).click();
 		
-		
+		logger.info("得到文本信息-----柠檬班题库APP后台管理系统");
 		WebDriverWait wait1 = new WebDriverWait(driver,10);
 		WebElement aElement1 = wait1.until(new ExpectedCondition<WebElement>() {
 			@Override
 			public WebElement apply(WebDriver arg0) {
-				
 				return driver.findElement(By.linkText("柠檬班题库APP后台管理系统"));
 			}
 		});
 		String actualResult = aElement1.getText();
-		System.out.println(actualResult);
 		
-		driver.findElement(By.xpath("//*[@id='menu-product']"));
+		logger.info("点击排课管理");
+		driver.findElement(By.xpath("//*[contains(@class,'bk_2')]/dl[1]/dt")).click();
 		
-		
-		WebDriverWait wait3 = new WebDriverWait(driver,10);
-		WebElement aElement3= wait3.until(new ExpectedCondition<WebElement>() {
+		logger.info("点击一周排课");
+		WebDriverWait wait2 = new WebDriverWait(driver,10);
+		WebElement aElement2= wait2.until(new ExpectedCondition<WebElement>() {
 			@Override
 			public WebElement apply(WebDriver arg0) {
 				
 				return driver.findElement(By.linkText("一周排课"));
 			}
 		});
-		aElement3.click();
+		aElement2.click();
 		
-		String text = driver.findElement(By.linkText(" 我要录课")).getText();
-		System.out.println(text);
+		logger.info("切换到对应的iframe");
+		driver.switchTo().frame(driver.findElement(By.cssSelector("iframe[src='class-plan-list.html']")));
+		
+		logger.info("找到批量删除按钮");
+		String text = driver.findElement(By.xpath("//*[contains(@class,'bk-gray')]/span/a[1]")).getText();
+		
+		logger.info("切换回默认的iframe");
+		driver.switchTo().defaultContent();
+		
+		logger.info("点击机构课程");
+		driver.findElement(By.xpath("//*[contains(@class,'bk_2')]/dl[2]/dt")).click();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
 		
 	}
 }
